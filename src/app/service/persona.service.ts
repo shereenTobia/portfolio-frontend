64994a30bf2c2a3dac922c4e4e15a51c4efd3017
Shereen <>
2023-09-22 22:46:48 -0300

    First commit to upload project

---
+++ src/app/service/persona.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { persona } from '../model/persona.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PersonaService {
+  URL =  'https://protestant-eugine-sshhery.koyeb.app/personas/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
+  }
+}
